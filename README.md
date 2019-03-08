ee7ed9491ea0f9910928e2de628b6dfd5f718890
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-03-08 12:52:02 +0100

    improved error handling; added instructions to readme

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Thumbnailer
 
-Some quick and dirty thumbnailer, hacked together in half an hour.
+Some quick and dirty thumbnailer based on ImageMagick.
+
+## Setup (ImageMagick)
+
+Make sure ImageMagick's policy file (`/etc/ImageMagick-7/policy.xml` on Arch
+Linux) allows reading PDFs by changing
+
+```xml
+<policy domain="coder" rights="none" pattern="{PS,PS2,PS3,EPS,PDF,XPS}" />
+```
+
+to
+
+
+```xml
+<policy domain="coder" rights="none" pattern="{PS,PS2,PS3,EPS,XPS}" />
+<policy domain="coder" rights="read" pattern="{PDF}" />
+```
+
+by excluding the PDF from the restrictive rule and creating a new rule for
+reading PDFs instead.
+
+## Execute
+
+Turn the first page of `demo.pdf` into `thumbnail.png`:
+
+```bash
+$ go run thumbnailer.go
+$ curl -X POST -F pdf=@demo.pdf localhost:1337/thumbnail > thumbnail.png
+```
--- thumbnailer.go
+++ thumbnailer.go
@@ -6,14 +6,15 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+	"strings"
 )
 
 const (
-	command = "/usr/bin/convert" // imagmagick
+	command = "/usr/bin/convert" // ImageMagick
 	density = "-density"
 	dpi     = "150"
 	resize  = "-resize"
-	width   = "200"
+	width   = "400"
 )
 
 func main() {
@@ -25,33 +26,50 @@ func thumbnail(w http.ResponseWriter, r *http.Request) {
 	file, _, err := r.FormFile("pdf")
 	defer file.Close()
 	if err != nil {
-		log.Print(err)
+		log.Printf("get 'pdf' file from multipart request: %v", err)
+		response(w, http.StatusBadRequest)
 		return
 	}
 
 	tempFile, err := ioutil.TempFile("", "*.pdf")
 	defer tempFile.Close()
 	if err != nil {
-		log.Print(err)
+		log.Printf("create temp file '%s' for source PDF: %v", err, tempFile.Name())
+		response(w, http.StatusInternalServerError)
 		return
 	}
 
-	io.Copy(tempFile, file)
+	_, err = io.Copy(tempFile, file)
+	if err != nil {
+		log.Printf("copy request file to '%s' failed: %v", tempFile.Name(), err)
+		response(w, http.StatusInternalServerError)
+		return
+	}
+
 	thumbnail, err := ioutil.TempFile("", "*.png")
 	defer thumbnail.Close()
 	if err != nil {
-		log.Print(err)
+		log.Printf("create temp file '%s' for thumbnail: %v", thumbnail.Name(), err)
+		response(w, http.StatusInternalServerError)
 		return
 	}
 
 	source := tempFile.Name() + "[0]" // [0] means first page
 	target := thumbnail.Name()
-	cmd := exec.Command(command, density, dpi, resize, width, source, target)
+	args := []string{density, dpi, resize, width, source, target}
+	cmd := exec.Command(command, args...)
 	err = cmd.Run()
 	if err != nil {
-		log.Print(err)
+		log.Printf("executing '%s %v': %v", command, strings.Join(args, " "), err)
+		response(w, http.StatusInternalServerError)
 		return
 	}
 
 	http.ServeFile(w, r, thumbnail.Name())
+}
+
+func response(w http.ResponseWriter, statusCode int) {
+	statusMessage := http.StatusText(statusCode)
+	w.WriteHeader(statusCode)
+	w.Write([]byte(statusMessage))
 }
