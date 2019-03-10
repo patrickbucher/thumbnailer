23d7fa2c986fc4fa34cd3651ea8b734d3425acd1
patrickbucher <patrick.bucher@stud.hslu.ch>
2019-03-10 19:02:40 +0100

    use width, height and density parameters from request

--- post-demo-pdf.sh
+++ post-demo-pdf.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X POST -F pdf=@demo.pdf localhost:1337/thumbnail > thumbnail.png
+curl -X POST -F width=500 -F height=708 -F density=300 -F pdf=@demo.pdf localhost:1337/thumbnail > thumbnail.png
--- thumbnailer.go
+++ thumbnailer.go
@@ -1,33 +1,36 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
-const (
-	command = "/usr/bin/convert" // ImageMagick
-	density = "-density"
-	dpi     = "150"
-	resize  = "-resize"
-	width   = "400"
-)
+const command = "/usr/bin/convert" // ImageMagick
 
 func main() {
 	http.HandleFunc("/thumbnail", thumbnail)
 	log.Fatal(http.ListenAndServe("0.0.0.0:1337", nil))
 }
 
-// TODO: accept density and resolution as parameters
 func thumbnail(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		log.Printf("called with method %s", r.Method)
 		response(w, http.StatusMethodNotAllowed)
+		return
+	}
+
+	args, err := parseParams(r)
+	if err != nil {
+		log.Printf("parsing params from multipart request: %v", err)
+		response(w, http.StatusBadRequest)
 		return
 	}
 
@@ -66,7 +69,8 @@ func thumbnail(w http.ResponseWriter, r *http.Request) {
 
 	source := tempFile.Name() + "[0]" // [0] means first page
 	target := thumbnail.Name()
-	args := []string{density, dpi, resize, width, source, target}
+	args = append(args, source)
+	args = append(args, target)
 	cmd := exec.Command(command, args...)
 	err = cmd.Run()
 	if err != nil {
@@ -76,10 +80,61 @@ func thumbnail(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.ServeFile(w, r, thumbnail.Name())
+	log.Printf("%s %s", command, strings.Join(args, " "))
 }
 
 func response(w http.ResponseWriter, statusCode int) {
 	statusMessage := http.StatusText(statusCode)
 	w.WriteHeader(statusCode)
 	w.Write([]byte(statusMessage))
+}
+
+func parseParams(r *http.Request) ([]string, error) {
+	params := make([]string, 0)
+	width, err := parseIntParam(r, "width")
+	if err != nil {
+		return params, err
+	}
+	height, err := parseIntParam(r, "height")
+	if err != nil {
+		return params, err
+	}
+	density, err := parseIntParam(r, "density")
+	if err != nil {
+		return params, err
+	}
+	if width > 0 || height > 0 {
+		params = append(params, "-resize")
+		widthStr, heightStr := "", ""
+		if width > 0 {
+			widthStr = strconv.Itoa(width)
+		}
+		if height > 0 {
+			heightStr = strconv.Itoa(height)
+		}
+		resizeParam := fmt.Sprintf("%sx%s", widthStr, heightStr)
+		if widthStr != "" && heightStr != "" {
+			// "!" ignores aspect ratio
+			resizeParam = fmt.Sprintf("%sx%s!", widthStr, heightStr)
+		}
+		params = append(params, resizeParam)
+	}
+	if density > 0 {
+		params = append(params, "-density")
+		params = append(params, strconv.Itoa(density))
+	}
+	return params, nil
+}
+
+func parseIntParam(r *http.Request, name string) (int, error) {
+	var intVal int
+	stringParam := r.FormValue(name)
+	if stringParam != "" {
+		i, err := strconv.Atoi(stringParam)
+		if err != nil {
+			return 0, errors.New(fmt.Sprintf("'%s' param not an integer: %v", name, err))
+		}
+		intVal = i
+	}
+	return intVal, nil
 }
