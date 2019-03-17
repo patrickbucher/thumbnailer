4d6220737e62601161d37ef3cd5ca2a0275d4203
patrickbucher <patrick.bucher@stud.hslu.ch>
2019-03-17 06:36:41 +0100

    first working version on OpenShift

--- openshift/openshift-cleanup.sh
+++ openshift/openshift-cleanup.sh
@@ -4,8 +4,3 @@ oc delete is/thumbnailer-base is/thumbnailer-s2i is/thumbnailer
 oc delete bc/thumbnailer-base bc/thumbnailer-s2i bc/thumbnailer
 oc delete dc/thumbnailer
 oc delete svc/thumbnailer
-
-echo 'wait for it...'
-sleep 5
-
-oc create -f deployment.yml
